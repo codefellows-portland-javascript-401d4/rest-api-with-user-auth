d77df24c491888055675074143ddd4a7f567c643
Christopher Bruner <chrbruner9@gmail.com>
2016-11-08 08:51:18 -0800

    added more tests for auth api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 const app = require('./lib/app');
 require('./lib/mongooseSetup');
 
-const server = http.CreateServer(app);
+const server = http.createServer(app);
 server.listen(port, () => {
     console.log('server listening on', server.address());
 });
