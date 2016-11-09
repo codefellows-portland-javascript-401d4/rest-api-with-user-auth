aa011c5af8bd2d5fb9d746fdbd0aaeb9126f14fb
Albert Reel <waxhoya@gmail.com>
2016-11-08 17:21:45 -0800

    Server debugged and now launches as intended

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./lib/app');
 const port = process.env.PORT || 3000;
 const http = require('http');
-require('.lib/mongoose-setup');
+require('./lib/mongoose-setup');
 
 const server = http.createServer(app);
 server.listen(port, () => {
