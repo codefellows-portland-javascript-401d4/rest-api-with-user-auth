44e4ae598809f32c3f159c5e71db4ce4e42f9e0d
Gloria Guy <totallygloria@gmail.com>
2016-11-09 17:09:14 -0800

    Added a test to hit a protected route using a token, currently failing.

--- test/auth-api.test.js
+++ test/auth-api.test.js
@@ -115,4 +115,19 @@ describe('User authentication routes', () => {
       });
   });
 
+  it.only('requires a token to hit the /me route', done => {
+
+    const expectedResults = '{"username":"Token User","favoriteAuthors":[],"favoriteBooks":[]}';
+
+    server
+      .get('/api/auth/me')
+      .set('Authorization', token)
+      .then(res => {
+        assert.equal(res.text, expectedResults);
+        done();
+      })
+      .catch(done);
+  });
+
+
 });
