6901b88968ab2df5a3743dbf052d486003cf5973
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-12-07 12:29:06 -0800

    Moved files from branch on relations lab repo

---
+++ test/request.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const app = require('../lib/app');
+chai.use(chaiHttp);
+
+module.exports = chai.request(app);
